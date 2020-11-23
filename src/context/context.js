c10d404cda1390823201648a9135275ff596f70a
haze355 <haze355@gmail.com>
2020-11-22 20:52:44 -0800

    get user info from github api

--- src/context/context.js
+++ src/context/context.js
@@ -20,6 +20,18 @@ const GithubProvider = ({ children }) => {
   // error
   const [error, setError] = useState({ show: false, msg: "" });
 
+  const searchGithubUser = async (user) => {
+    toggleError();
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+  };
+
   //  check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -44,7 +56,15 @@ const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ githubUser, repos, followers, requests, error }}
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
     >
       {children}
     </GithubContext.Provider>
